a84ddb60f06e8e1370b530ae3d54f95e86099d67
elygarcia023 <garciaelyzzer@gmail.com>
2024-06-04 01:12:09 -1100

    Added Home/LandingPage

--- script.js
+++ script.js
@@ -1,3 +1,71 @@
+/* Home */
+let next = document.querySelector(".next");
+let prev = document.querySelector(".prev");
+
+next.addEventListener('click', function() {
+    let items = document.querySelectorAll('.item');
+    document.querySelector('.slide').appendChild(items[0]);
+});
+
+prev.addEventListener('click', function() {
+    let items = document.querySelectorAll('.item');
+    document.querySelector('.slide').prepend(items[items.length - 1]);
+});
+
+const observerOptions = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.1
+};
+
+const revealCallback = (entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('reveal');
+            observer.unobserve(entry.target);
+        }
+    });
+};
+
+
+var swiper = new Swiper(".swiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 3,
+    slideShadows: true
+  },
+  keyboard: {
+    enabled: true
+  },
+  mousewheel: {
+    thresholdDelta: 70
+  },
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2
+    },
+    768: {
+      slidesPerView: 1
+    },
+    1024: {
+      slidesPerView: 2
+    },
+    1560: {
+      slidesPerView: 3
+    }
+  }
+});
+/* Home */
 function showSection(sectionId) {
   document.querySelectorAll('section').forEach(function(section) {
       section.style.display = 'none';
